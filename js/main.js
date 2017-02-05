74c048149b83eee7fc033fc23ceec1bd14561d4f
tim seo <gdurtyfive@gmail.com>
2017-02-04 21:06:48 -0800

    added moski logos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log("your #1 place for pregame")
+
+
+window.onLoad = function () {
+  var canvas = document.getElementById('playArea'),
+    c = canvas.getContext("2d");
+
+    c.fillStyle = "red";
+    c.fillRect = (0, 0, canvas.width, canvas.height);
+}
