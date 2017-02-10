96a16ce015270143af263c3aa82fb9daad8f3aee
timseo <gdurtyfive@gmail.com>
2017-02-09 22:46:39 -0800

    added scoreboard indicator

--- js/main.js
+++ js/main.js
@@ -8,11 +8,7 @@ $(document).ready(function() {
   ascore = 1;
   bscore = 1;
 
-  // var matchOriginal = $("#match").clone();
-  // var playerOneWinsOriginal = $("#playerOneWins").clone();
-  // var playerTwoWinsOriginal = $("#playerTwoWins").clone();
-
-
+  // var nameAlpha = prompt("You are Also Known As_________?").toUpperCase();
 
 
   function rollDice(min, max) {
@@ -34,12 +30,15 @@ $(document).ready(function() {
     if ($diceOne === $diceTwo) {
       return $('#messageBox').html("WINNER! <br>  <h3>Bravo DRINK! <br>   KEEP ROLLING Alpha!</h3>"),
         $('#playerTwoWins').html(ascore++);
+  ;
     } else if ($shooterOneScore.text() == 7) {
       return $('#messageBox').html("WINNER! <br>  <h3>Bravo DRINK! <br>   KEEP ROLLING Alpha!</h3>"),
         $('#playerTwoWins').html(ascore++);
+
     } else if ($shooterOneScore.text() == 11) {
       return $('#messageBox').html("WINNER! <br>  <h3>Bravo DRINK! <br>   KEEP ROLLING Alpha!</h3>"),
         $('#playerTwoWins').html(ascore++);
+
     } else {
       $('#buttonOne').css('visibility', 'hidden')
       $('#buttonTwo').css('visibility', 'visible')
@@ -66,6 +65,7 @@ $(document).ready(function() {
     } else if ($shooterTwoScore.text() == 11) {
       return $('#messageBox').html("WINNER!  <br> <h3>Alpha DRINK! <br>    KEEP ROLLING BRAVO!</h3>"),
         $('#playerOneWins').html(bscore++);
+
     } else {
       $('#buttonTwo').css('visibility', 'hidden')
       $('#buttonOne').css('visibility', 'visible')
@@ -74,28 +74,26 @@ $(document).ready(function() {
   })
 
 
-    function gameWinner() {
-      if (bscore === 5) {
+  function gameWinner() {
+    if (bscore === 5) {
       $('#match').text("Bravo wins while Alpha blacks out!");
-      setTimeout(playAgain, 1000)
-      // $('#match').append($'<button {id="restartButton"}>Play Again?</button>');
+      setTimeout(playAgain, 3000)
 
     } else if (ascore === 5) {
       $('#match').text("Alpha wins while Bravo blacks out!");
-      setTimeout(playAgain, 1000)
-      // $('#match').append($'<button {id="restartButton"}>Play Again?</button>');
+      setTimeout(playAgain, 3000)
     }
 
-}
+  }
 
-    function playAgain() {
-      $("#match").text('Tournament rules:  1st shooter to parse out 5 drinks WINS!');
-      $("#match").append('<br> <br> <div id="playerOneWins">0</div> <span id="tournamentColon">:</span> <div id="playerTwoWins">0</div>');
-      $('#buttonOne').css('visibility', 'visible');
-      $('#buttonTwo').css('visibility', 'visible')
-      ascore = 1;
-      bscore = 1;
-    }
+  function playAgain() {
+    $("#match").text('Tournament rules:  1st shooter to parse out 5 drinks WINS!');
+    $("#match").append('<br> <br> <div id="playerOneWins"><span id="playerAlpha">Alpha :  </span>0</div> <span id="tournamentColon">:</span> <div id="playerTwoWins">0<span id="playerBravo">  : Bravo</span></div>');
+    $('#buttonOne').css('visibility', 'visible');
+    $('#buttonTwo').css('visibility', 'visible');
+    ascore = 1;
+    bscore = 1;
+  }
 
 
 
