ecac6de8976e3438164e470f57f036a8199b1c1c
timseo <gdurtyfive@gmail.com>
2017-02-09 16:18:20 -0800

    in process of adding reset button

--- js/main.js
+++ js/main.js
@@ -7,7 +7,12 @@ $(document).ready(function() {
   $bravoScore = $('#playerTwoWins');
   ascore = 1;
   bscore = 1;
-  var rollTotal = 0;
+  var matchOriginal = $("#match").clone();
+  var playerOneWinsOriginal = $("#playerOneWins").clone();
+  var playerTwoWinsOriginal = $("#playerTwoWins").clone();
+
+
+
 
   function rollDice(min, max) {
     min = Math.ceil(min);
@@ -16,40 +21,31 @@ $(document).ready(function() {
   }
 
 
-        $('#buttonOne').on('click', function() {
-          $('#diceOne').html(rollDice(1, 6));
-          $('#diceTwo').html(rollDice(1, 6));
-          var $shooterOneScore = $('#diceTotal').eq(0);
-          var $diceOne = $('#diceOne').text();
-          var $diceTwo = $('#diceTwo').text();
-          $shooterOneScore.html(parseInt($diceOne) + parseInt($diceTwo));
+  $('#buttonOne').on('click', function() {
+    $('#diceOne').html(rollDice(1, 6));
+    $('#diceTwo').html(rollDice(1, 6));
+    var $shooterOneScore = $('#diceTotal').eq(0);
+    var $diceOne = $('#diceOne').text();
+    var $diceTwo = $('#diceTwo').text();
+    $shooterOneScore.html(parseInt($diceOne) + parseInt($diceTwo));
+    gameWinner();
 
-          if ($diceOne === $diceTwo) {
-            return $('#messageBox').html("WINNER! <br>  <h3>Bravo DRINK! <br>   KEEP ROLLING Alpha!</h3>"),
-            $('#playerTwoWins').html(ascore++);
-
-
-
-          } else if ($shooterOneScore.text() == 7) {
-            return $('#messageBox').html("WINNER! <br>  <h3>Bravo DRINK! <br>   KEEP ROLLING Alpha!</h3>"),
-            $('#playerTwoWins').html(ascore++);
-
-
-          } else if ($shooterOneScore.text() == 11) {
-            return $('#messageBox').html("WINNER! <br>  <h3>Bravo DRINK! <br>   KEEP ROLLING Alpha!</h3>"),
-            $('#playerTwoWins').html(ascore++);
-
-
-          } else {
-            $('#buttonOne').css('visibility','hidden')
-            $('#buttonTwo').css('visibility','visible')
-            return $('#messageBox').html("Alpha loses <br> <h3>your turn Bravo  Shoot em!</h3>");
-            return $('#messageBox').text('');
-
-          }
+    if ($diceOne === $diceTwo) {
+      return $('#messageBox').html("WINNER! <br>  <h3>Bravo DRINK! <br>   KEEP ROLLING Alpha!</h3>"),
+        $('#playerTwoWins').html(ascore++);
+    } else if ($shooterOneScore.text() == 7) {
+      return $('#messageBox').html("WINNER! <br>  <h3>Bravo DRINK! <br>   KEEP ROLLING Alpha!</h3>"),
+        $('#playerTwoWins').html(ascore++);
+    } else if ($shooterOneScore.text() == 11) {
+      return $('#messageBox').html("WINNER! <br>  <h3>Bravo DRINK! <br>   KEEP ROLLING Alpha!</h3>"),
+        $('#playerTwoWins').html(ascore++);
+    } else {
+      $('#buttonOne').css('visibility', 'hidden')
+      $('#buttonTwo').css('visibility', 'visible')
+      return $('#messageBox').html("Alpha loses <br> <h3>your turn Bravo  Shoot em!</h3>");
+    }
 
   })
-
 
   $('#buttonTwo').on('click', function() {
     $('#diceOne').html(rollDice(1, 6));
@@ -58,47 +54,42 @@ $(document).ready(function() {
     var $diceOne = $('#diceOne').text();
     var $diceTwo = $('#diceTwo').text();
     $shooterTwoScore.html(parseInt($diceOne) + parseInt($diceTwo));
+    gameWinner();
 
     if ($diceOne === $diceTwo) {
       return $('#messageBox').html("WINNER! <br> <h3>Alpha DRINK! <br>    KEEP ROLLING BRAVO!</h3>"),
-      $('#playerOneWins').html(bscore++);
-
+        $('#playerOneWins').html(bscore++);
     } else if ($shooterTwoScore.text() == 7) {
       return $('#messageBox').html("WINNER! <br> <h3>Alpha DRINK! <br>    KEEP ROLLING BRAVO!</h3>"),
-      $('#playerOneWins').html(bscore++);
-
+        $('#playerOneWins').html(bscore++);
     } else if ($shooterTwoScore.text() == 11) {
       return $('#messageBox').html("WINNER!  <br> <h3>Alpha DRINK! <br>    KEEP ROLLING BRAVO!</h3>"),
-      $('#playerOneWins').html(bscore++);
-
+        $('#playerOneWins').html(bscore++);
     } else {
-      $('#buttonTwo').css('visibility','hidden')
-      $('#buttonOne').css('visibility','visible')
+      $('#buttonTwo').css('visibility', 'hidden')
+      $('#buttonOne').css('visibility', 'visible')
       return $('#messageBox').html("Bravo Loses <br> <h3>your turn Alpha  Shoot em!</h3>");
-      return $('#messageBox').text('');
+    }
+  })
+
+
+    function gameWinner() {
+      if (bscore === 3) {
+      $('#match').text("Bravo wins while Alpha blacks out!");
+      // $('#match').append($'<button {id="restartButton"}>Play Again?</button>');
+
+    } else if (ascore === 3) {
+      $('#match').text("Alpha wins while Bravo blacks out!");
+      // $('#match').append($'<button {id="restartButton"}>Play Again?</button>');
     }
 
-    //switch function here
+}
 
-  })
-  // rollDiceTwo(1,6)
-
-
-
-
-
-  // $('#diceOne').text() + $('#diceTwo').text());
-  // gameWinner(shooter1)
-  // gameWinner(shooter2)
-
-
-
-  // tournament winner logic
-
-
-
-
-
+    function playAgain() {
+      $("#playerTwoWins").replaceWith(playerTwoWinsOriginal.clone());
+      $("#playerOneWins").replaceWith(playerOneWinsOriginal.clone());
+      $("#match").replaceWith(matchOriginal.clone());
+    }
 
 
 
